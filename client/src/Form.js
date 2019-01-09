5d783bdafd1e2ef4a230482a3c2c49e11bd652b2
mning1598 <mning1598@gmail.com>
2019-01-08 19:04:50 -0500

    Todo List

---
+++ client/src/Form.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+
+
+export default class Form extends React.Component {
+
+	state = {
+		text: ""
+	};
+
+	handleChange = e => {
+		const newText = e.target.value;
+		this.setState({
+			text: newText
+		});
+	};
+
+	handleKeyDown = e => {
+		console.log(e);
+		if(e.key === 'Enter') {
+			this.props.submit(this.state.text);
+			this.setState({text: ""})
+		}
+	};
+
+	render() {
+		const {text} = this.state;
+		return (
+			<TextField
+				onChange={this.handleChange}
+				onKeyDown={this.handleKeyDown}
+				label = "todo..."
+				value={text}
+				margin="normal"
+				fullWidth
+			/>
+		);
+	}
+}
